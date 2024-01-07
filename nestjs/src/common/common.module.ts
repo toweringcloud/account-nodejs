bad16e1c6bdf0c77a3f583e42898fde5f741735b
toweringcloud <toweringcloud@gmail.com>
2024-01-08 01:43:01 +0900

    [MOD] jwt service -> common module

--- nestjs/src/common/common.module.ts
+++ nestjs/src/common/common.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
 import { PrismaService } from './common.service';
 
 @Module({
-  exports: [PrismaService],
-  providers: [PrismaService],
+  exports: [PrismaService, JwtService],
+  providers: [PrismaService, JwtService],
 })
 export class CommonModule {}
--- nestjs/src/users/users.module.ts
+++ nestjs/src/users/users.module.ts
@@ -3,12 +3,11 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from 'src/common/common.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [CommonModule],
   controllers: [UsersController],
-  providers: [UsersService, JwtService],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
